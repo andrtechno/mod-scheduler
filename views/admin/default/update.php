6c80e626e93b80a56d99a556b981e5f72220d401
Andrew <andrew.panix@gmail.com>
2024-08-25 19:17:08 +0300

    init

--- views/admin/default/update.php
+++ views/admin/default/update.php
@@ -17,97 +17,87 @@ $this->title = $model->__toString();
 $this->params['breadcrumbs'][] = ['label' => SchedulerTask::label(2), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $model->__toString();
 ?>
-<div class="task-update">
-    <div class="row">
-        <div class="col-lg-6">
 
-            <div class="card">
-                <div class="card-header">
-                    <h5><?= $this->title ?></h5>
-                </div>
-                <div class="card-body">
+<div class="row">
+    <div class="col-lg-6">
+        <?php $form = ActiveForm::begin([
+            'id' => $model->formName(),
+            'layout' => 'horizontal',
+            'enableClientValidation' => false,
+        ]); ?>
 
+        <div class="card">
+            <div class="card-header">
+                <h5><?= $this->title ?></h5>
+            </div>
+            <div class="card-body">
 
+                <?= $form->field($model, 'name', ['inputOptions' => ['disabled' => 'disabled']]) ?>
+                <?= $form->field($model, 'description', ['inputOptions' => ['disabled' => 'disabled']]) ?>
+                <?= $form->field($model, 'schedule', ['inputOptions' => ['disabled' => 'disabled']]) ?>
+                <?= $form->field($model, 'status', ['inputOptions' => ['disabled' => 'disabled']]) ?>
 
+                <?php if ($model->status_id == SchedulerTask::STATUS_RUNNING): ?>
+                    <?= $form->field($model, 'started_at', ['inputOptions' => ['disabled' => 'disabled']]) ?>
+                <?php endif ?>
 
-                    <?php $form = ActiveForm::begin([
-                        'id' => $model->formName(),
-                        'layout' => 'horizontal',
-                        'enableClientValidation' => false,
-                    ]); ?>
+                <?= $form->field($model, 'last_run', ['inputOptions' => ['disabled' => 'disabled']]) ?>
+                <?= $form->field($model, 'next_run', ['inputOptions' => ['disabled' => 'disabled']]) ?>
 
-                    <?= $form->field($model, 'name', ['inputOptions' => ['disabled' => 'disabled']]) ?>
-                    <?= $form->field($model, 'description', ['inputOptions' => ['disabled' => 'disabled']]) ?>
-                    <?= $form->field($model, 'schedule', ['inputOptions' => ['disabled' => 'disabled']]) ?>
-                    <?= $form->field($model, 'status', ['inputOptions' => ['disabled' => 'disabled']]) ?>
-
-                    <?php if ($model->status_id == SchedulerTask::STATUS_RUNNING): ?>
-                        <?= $form->field($model, 'started_at', ['inputOptions' => ['disabled' => 'disabled']]) ?>
-                    <?php endif ?>
-
-                    <?= $form->field($model, 'last_run', ['inputOptions' => ['disabled' => 'disabled']]) ?>
-                    <?= $form->field($model, 'next_run', ['inputOptions' => ['disabled' => 'disabled']]) ?>
-
-                    <?= $form->field($model, 'active')->dropdownList([1 => 'Yes', 0 => 'No']);
-                    ?>
-
-                    <?= Html::submitButton('<span class="glyphicon glyphicon-check"></span> ' . ($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Save')), [
-                        'id' => 'save-' . $model->formName(),
-                        'class' => 'btn btn-primary'
-                    ]); ?>
-
-                    <?php ActiveForm::end(); ?>
-
-
-                </div>
+                <?= $form->field($model, 'active')->dropdownList([1 => Yii::t('yii', 'Yes'), 0 => Yii::t('yii', 'No')]);
+                ?>
+            </div>
+            <div class="card-footer">
+                <?= Html::submitButton(($model->isNewRecord ? Yii::t('app/default', 'CREATE') : Yii::t('app/default', 'UPDATE')), [
+                    'id' => 'save-' . $model->formName(),
+                    'class' => 'btn btn-primary'
+                ]); ?>
             </div>
         </div>
-        <div class="col-lg-6">
-
-
-            <div class="table-responsive">
-                <?php \yii\widgets\Pjax::begin(['id' => 'logs']); ?>
-                <?= GridView::widget([
-                    'layout' => '{summary}{pager}{items}{pager}',
-                    'layoutOptions' => ['title' => 'Logs'],
-                    'dataProvider' => $logDataProvider,
-                    'pager' => [
-                        'class' => yii\widgets\LinkPager::className(),
-                        'firstPageLabel' => Yii::t('app', 'First'),
-                        'lastPageLabel' => Yii::t('app', 'Last'),
+        <?php ActiveForm::end(); ?>
+    </div>
+    <div class="col-lg-6">
+        <div class="table-responsive">
+            <?php \yii\widgets\Pjax::begin(['id' => 'logs']); ?>
+            <?= GridView::widget([
+                'layout' => '{summary}{pager}{items}{pager}',
+                'layoutOptions' => ['title' => 'Logs'],
+                'dataProvider' => $logDataProvider,
+                'pager' => [
+                    'class' => yii\widgets\LinkPager::className(),
+                    'firstPageLabel' => Yii::t('app', 'First'),
+                    'lastPageLabel' => Yii::t('app', 'Last'),
+                ],
+                'columns' => [
+                    [
+                        'attribute' => 'started_at',
+                        'format' => 'raw',
+                        'value' => function (SchedulerLog $model) {
+                            return Html::a(Yii::$app->getFormatter()->asDatetime($model->started_at), ['view-log', 'id' => $model->id], ['data-pjax' => 0]);
+                        }
                     ],
-                    'columns' => [
-                        [
-                            'attribute' => 'started_at',
-                            'format' => 'raw',
-                            'value' => function (SchedulerLog $model) {
-                                return Html::a(Yii::$app->getFormatter()->asDatetime($model->started_at), ['view-log', 'id' => $model->id], ['data-pjax' => 0]);
-                            }
-                        ],
-                        'ended_at:datetime',
-                        [
-                            'label' => 'Duration',
-                            'value' => function (SchedulerLog $model) {
-                                return $model->getDuration();
-                            }
-                        ],
-                        [
-                            'label' => 'Result',
-                            'format' => 'raw',
-                            'contentOptions' => ['class' => 'text-center'],
-                            'value' => function (SchedulerLog $model) {
-
-                                return Html::tag('span', '', [
-                                    'class' => $model->error == 0 ? 'text-success icon-check' : 'text-danger icon-warning'
-                                ]);
-                            }
-                        ],
+                    'ended_at:datetime',
+                    [
+                        'label' => 'Duration',
+                        'value' => function (SchedulerLog $model) {
+                            return $model->getDuration();
+                        }
                     ],
-                ]); ?>
-                <?php \yii\widgets\Pjax::end(); ?>
-            </div>
+                    [
+                        'label' => 'Result',
+                        'format' => 'raw',
+                        'contentOptions' => ['class' => 'text-center'],
+                        'value' => function (SchedulerLog $model) {
 
+                            return Html::tag('span', '', [
+                                'class' => $model->error == 0 ? 'text-success icon-check' : 'text-danger icon-warning'
+                            ]);
+                        }
+                    ],
+                ],
+            ]); ?>
+            <?php \yii\widgets\Pjax::end(); ?>
         </div>
     </div>
-
 </div>
+
